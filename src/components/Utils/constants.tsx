507c70b70254e6bd4f2c9a263f9a70faa8663cf8
Diogo <diogo_cpmg@hotmail.com>
2020-12-29 11:15:46 -0300

    create files dev, prod and create a component Utils

---
+++ src/components/Utils/constants.tsx
@@ -0,0 +1,6 @@
+const SERVER = process.env.REACT_APP_SERVER;
+
+export const AUTH_ENDPOINT = `${SERVER}`;
+export const API_ENDPOINT = `${SERVER}/api`;
+export const JWT_TOKEN_NAME = "jwt";
+export const CREDENTIALS_NAME = "credentials"; 
