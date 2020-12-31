3941a713406925bdf2e0655cc2b6e5310a889641
Diogo <diogo_cpmg@hotmail.com>
2020-12-31 12:08:02 -0300

    component Utils deleted, organizing structure

--- src/components/Utils/constants.tsx
+++
@@ -1,6 +0,0 @@
-const SERVER = process.env.REACT_APP_SERVER;
-
-export const AUTH_ENDPOINT = `${SERVER}`;
-export const API_ENDPOINT = `${SERVER}/api`;
-export const JWT_TOKEN_NAME = "jwt";
-export const CREDENTIALS_NAME = "credentials"; 
